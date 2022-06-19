9be00b388d8f5e05e867dc6b393107381129b6ec
YangYGYang <ginayoung82@gmail.com>
2022-06-19 19:44:09 +0800

    without css

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-    res.send('restaurant server')
+    res.render('index')
 })
 
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
+
+// setting static files
+app.use(express.static('public'))
