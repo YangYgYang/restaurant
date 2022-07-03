f4e95dbcf837e63b2252f96077c6c86da76cdda5
YangYGYang <ginayoung82@gmail.com>
2022-07-03 13:45:07 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const restaurantModel = require('./models/restaurantExample')
 const routes = require('./routes')
 
 //在ROBO 3T上新增DB關掉後，會就不見了？ 
-const mongoose = require('mongoose')
+
 
 const hbs = exphbs.create({
     // Specify helpers which are only registered on this instance.
@@ -20,16 +20,21 @@ const hbs = exphbs.create({
     defaultLayout: 'main'
 });
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
+// const db = mongoose.connection
 
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
+// db.on('error', () => {
+//     console.log('mongodb error!')
+// })
+// db.once('open', () => {
+//     console.log('mongodb connected!')
+// })
+//==========引入mongoose
+require('./config/mongoose')
+
+
 
 //==========中介軟體 設定
 const bodyParser = require('body-parser')
@@ -43,85 +48,6 @@ app.set('view engine', 'handlebars')
 
 app.use(methodOverride('_method'))
 app.use(routes)
-
-// app.get('/', (req, res) => {
-//     restaurantModel.find()
-//         .lean()
-//         .then((restaurants) => { res.render('index', { restaurants }) })
-//         .catch(error => { console.log('error') })
-// })
-
-// app.get('/restaurants/:_id/detail', (req, res) => {
-//     const id = req.params._id
-//     restaurantModel.findById(id)
-//         .lean()
-//         .then((restaurant) => { res.render('show', { restaurant: restaurant }) })
-//         .catch(error => console.log('error'))
-// })
-
-// app.get('/restaurants/search', (req, res) => {
-//     const keyword = req.query.keyword
-//     restaurantModel.find()
-//         .lean()
-//         .then((restaurantData) => {
-//             const restaurants = restaurantData.filter((item) => {
-//                 return (
-//                     item.name.toLowerCase().includes(keyword.toLowerCase()) ||
-//                     item.category.toLowerCase().includes(keyword.toLowerCase()) ||
-//                     item.description.toLowerCase().includes(keyword.toLowerCase())
-//                 )
-//             })
-//             res.render('index', { restaurants: restaurants, keyword: keyword })
-//         })
-//         .catch(error => 'error')
-// })
-
-// app.get('/restaurants/:id/edit', (req, res) => {
-//     console.log('有進到edit路由')
-//     const id = req.params.id
-//     restaurantModel.findById(id)
-//         .lean()
-//         .then((restaurants) => {
-//             res.render('edit', { restaurants })
-//         })
-//         .catch(error => console.log('error', error))
-// })
-
-// app.put('/restaurants/:_id', (req, res) => {
-//     const id = req.params._id
-//     const data = req.body
-//     return restaurantModel.findById(id)
-//         .then((restaurants) => {
-//             for (let changeKey in data) {
-//                 restaurants[changeKey] = data[changeKey]
-//             }
-//             return restaurants.save()
-//         })
-//         .then(() => res.redirect(`/restaurants/${id}/detail`))
-//         .catch(error => console.log('error', error))
-// })
-
-// app.delete('/restaurants/:_id', (req, res) => {
-//     const id = req.params._id
-//     return restaurantModel.findById(id)
-//         .then(restaurant => { restaurant.remove() })
-//         .then(() => res.redirect('/'))
-//         .catch(error => console.log(error))
-// })
-
-// app.get('/restaurants/create', (req, res) => {
-//     return res.render('create')
-// })
-
-// app.post('/restaurants/create', (req, res) => {
-//     console.log('有進到create')
-//     const restaurant = req.body
-//     console.log(restaurant)
-//     return restaurantModel.create(restaurant)
-//         .then(() => { res.redirect('/') })
-//         .catch(error => console.log(error))
-// })
-
 
 
 
--- models/seeds/restaurantsSeeder.js
+++ models/seeds/restaurantsSeeder.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose')
 const restaurantSeed = require('../restaurant.json')
 const restaurantModel = require('../restaurantExample.js')
+const db = require('../../config/mongoose')
 
-mongoose.connect(process.env.MONGODB_URL)
-const db = mongoose.connection
-
-db.on('error', () => {
-    console.log('MONGODB error!')
-})
 db.once('open', () => {
     let restaurantData = restaurantSeed.results
     restaurantData.forEach((item) => {
