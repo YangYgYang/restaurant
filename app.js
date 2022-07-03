7b20fb0d4955b1d79d4b49aa310fe453e234a6d1
YangYGYang <ginayoung82@gmail.com>
2022-07-03 13:58:29 +0800

    add comment

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
+//==========setting server
 const express = require('express')
 const app = express()
 const port = 3000
+
+
+//==========setting handlebars config
 const exphbs = require('express-handlebars')
-const restaurantModel = require('./models/restaurantExample')
-const routes = require('./routes')
-
-//在ROBO 3T上新增DB關掉後，會就不見了？ 
-
-
 const hbs = exphbs.create({
     // Specify helpers which are only registered on this instance.
     helpers: {
@@ -20,20 +18,8 @@ const hbs = exphbs.create({
     defaultLayout: 'main'
 });
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// const db = mongoose.connection
-
-// db.on('error', () => {
-//     console.log('mongodb error!')
-// })
-// db.once('open', () => {
-//     console.log('mongodb connected!')
-// })
 //==========引入mongoose
 require('./config/mongoose')
-
 
 
 //==========中介軟體 設定
@@ -41,19 +27,19 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 const methodOverride = require('method-override')
 
-//setting template engine
+//==========setting template engine
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-
+//==========setting router
+const routes = require('./routes')
 app.use(methodOverride('_method'))
 app.use(routes)
-
 
 
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
 
-// setting static files
+//==========setting static files
 app.use(express.static('public'))
